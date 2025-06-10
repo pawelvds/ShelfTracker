2223a127f842ded643f6a993f2df5b34fc255837
pawelvds <karczewskipawel.j@gmail.com>
2025-06-11 00:05:33 +0200

    Added initial migration and setup for Book entity

--- ShelfTracker/Program.cs
+++ ShelfTracker/Program.cs
@@ -1,17 +1,22 @@
+using Microsoft.EntityFrameworkCore;
+using ShelfTracker.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
